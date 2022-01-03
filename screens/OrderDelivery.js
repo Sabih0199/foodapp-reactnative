60e42d5315e689db5b5c2803bd3417bd6935781a
Sabih0199 <sabihali1k99@gmail.com>
2022-01-03 17:33:16 +0500

    initial commit

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const OrderDelivery = () => {
+  return (
+    <View>
+      <Text>OrderDelivery Page</Text>
+    </View>
+  );
+};
+
+export default OrderDelivery;
---
+++ screens/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Restaurant = () => {
+  return (
+    <View>
+      <Text>Restaurant Page</Text>
+    </View>
+  );
+};
+
+export default Restaurant;
